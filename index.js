e8dc1d1996eb9724f74177538c3507cba6642aeb
JustinToyomitsu <w4tson442@gmail.com>
2020-11-13 16:38:07 -0800

    This is the base files to get started

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+ * Created By: Justin Toyomitsu
+ * email: ichitsurume@gmail.com
+ * Date Created: November 13th 2020
+ * Last Modified:
+ * =====================================================
+ */
+
+//This is the javascript for the main page
+//1. Main page to enter your gmail for google integration
+//2. redirect to a dashboard of all the informatino
+//3. 
+
+function gmail_submit(){ /* submit button goes here */
+
+}
+
+function gmail_validate(){ /* validate gmail */
+
+}
+
+function gmail_final(){ /* goes to the backend for python to send to the GoogleAPI */
+
+}

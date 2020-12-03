80c3dad2ba7a1066ed2c1308eceeef15d2e21a9b
root <ichitsurume@gmail.com>
2020-12-03 22:32:35 +0000

    Connecting mysql with Django

--- Googlio/start.sh
+++ Googlio/start.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
+sudo mysql -e "create database isekai;"
+sudo mysql -e "create user 'isekai'@'localhost' identified by 'test123';"
+sudo mysql -e "grant all privileges on isekai.* to 'isekai'@'localhost';"
+sudo mysql -e "flush privileges;"
+
 virtualenv project_env
 source project_env/bin/activate
 pip3 install Django
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y libssl-dev
+pip3 install mysqlclient
 deactivate
 
 sudo rm /etc/apache2/sites-available/000-default.conf

683fa130bc4ab4c0ff38b1610975ed9f4d8c21c8
JustinToyomitsu <w4tson442@gmail.com>
2020-12-08 16:54:18 -0800

    Adding file changes to add emails in admin block

--- Googlio/front/models.py
+++ Googlio/front/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    userId = models.CharField(unique=True, max_length=200)
+class Email(models.Model):
+    userName = models.CharField(unique=True, max_length=200)
     gmail = models.CharField(max_length=200)
     create_date = models.DateTimeField('Create Date', auto_now_add=True)
     last_updated = models.DateTimeField('Last Updated', auto_now=True)
